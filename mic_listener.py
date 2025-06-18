17152c1d619a2225e7d80f5618113393d5a28da1
JARVIS-AI-PI <jarvis.ai.ras.pi@gmail.com>
2025-06-18 16:23:03 +0530

    Delete mic_listener.py

--- mic_listener.py
+++
@@ -1,8 +0,0 @@
-import vosk, sys, json, sounddevice as sd
-model = vosk.Model("/home/pi/Desktop/Jarvis/voice/vosk-model")
-with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16', channels=1) as stream:
-    rec = vosk.KaldiRecognizer(model, 16000)
-    while True:
-        data = stream.read(4000)
-        if rec.AcceptWaveform(data):
-            print(json.loads(rec.Result())['text'])
