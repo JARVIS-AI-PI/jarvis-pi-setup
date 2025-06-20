74186c249d4998db956936b34bfab7a1f1495a5e
JARVIS-AI-PI <jarvis.ai.ras.pi@gmail.com>
2025-06-20 09:00:11 +0530

    index.js

--- modules/index.js/index.js
+++ modules/index.js/index.js
@@ -1,32 +1,16 @@
-// modules/index.js/index.js
-
 const fs = require('fs');
 const path = require('path');
 
-const modules = {};
-const modulesPath = path.join(__dirname, '..');
+const modules = [];
 
-// List all folders in /modules
-const moduleDirs = fs.readdirSync(modulesPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
-
-// Try to load each module's .js file
-for (const moduleName of moduleDirs) {
-    const moduleFile = path.join(modulesPath, moduleName, `${moduleName}.js`);
-    if (fs.existsSync(moduleFile)) {
-        try {
-            const loaded = require(moduleFile);
-            if (typeof loaded.respond === 'function') {
-                modules[moduleName] = loaded;
-                console.log(`[Loader] Module loaded: ${moduleName}`);
-            } else {
-                console.warn(`[Loader] Skipped ${moduleName}: No 'respond' function`);
-            }
-        } catch (err) {
-            console.error(`[Loader] Error loading module "${moduleName}":`, err.message);
-        }
+fs.readdirSync(__dirname).forEach(folder => {
+  const modPath = path.join(__dirname, folder);
+  if (fs.lstatSync(modPath).isDirectory()) {
+    const file = path.join(modPath, `${folder}.js`);
+    if (fs.existsSync(file)) {
+      modules.push(require(file));
     }
-}
+  }
+});
 
 module.exports = modules;
