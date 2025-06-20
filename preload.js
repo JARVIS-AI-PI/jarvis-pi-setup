7fce7b0031ec3078ede8ca2944b72e7b4e076067
JARVIS-AI-PI <jarvis.ai.ras.pi@gmail.com>
2025-06-20 14:18:04 +0530

    preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
-// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('jarvis', {
-  process: async (input) => await ipcRenderer.invoke('process-input', input),
+  process: (input) => ipcRenderer.invoke('process-input', input),
   speak: (text) => ipcRenderer.send('speak-text', text),
+  onVoiceOutput: (callback) => ipcRenderer.on('voice-output', (event, data) => callback(data))
 });
