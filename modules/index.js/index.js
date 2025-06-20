6ece01e877c716a095df996a9d5f31a3ecc4e746
JARVIS-AI-PI <jarvis.ai.ras.pi@gmail.com>
2025-06-20 14:15:00 +0530

    index.js

--- modules/index.js/index.js
+++ modules/index.js/index.js
@@ -1,15 +1,15 @@
+// modules/index.js
 const fs = require('fs');
 const path = require('path');
 
+const modulesDir = __dirname;
 const modules = [];
 
-fs.readdirSync(__dirname).forEach(folder => {
-  const modPath = path.join(__dirname, folder);
-  if (fs.lstatSync(modPath).isDirectory()) {
-    const file = path.join(modPath, `${folder}.js`);
-    if (fs.existsSync(file)) {
-      modules.push(require(file));
-    }
+fs.readdirSync(modulesDir, { withFileTypes: true }).forEach(dirent => {
+  if (!dirent.isDirectory()) return;
+  const file = path.join(modulesDir, dirent.name, `${dirent.name}.js`);
+  if (fs.existsSync(file)) {
+    modules.push(require(file));
   }
 });
 
